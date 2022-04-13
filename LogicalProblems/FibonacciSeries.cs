fc0ab26f5b0e46aa6e798a25ec3b6598f4230fb8
Harshal Arun Patil <harshalapatil2796@gmail.com>
2022-04-13 18:33:17 +0530

    [Harshal]Add:Fibonacci Series Task is Done

---
+++ LogicalProblems/FibonacciSeries.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProblems
+{
+    public class FibonacciSeries
+    {
+        public void Fibonacci()
+        {
+            int a = 0, b = 1, c;
+            Console.WriteLine("Enter the number of terms:");
+            int num = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= num; i++)
+            {
+
+                c = a + b;
+                a = b;
+                b = c;
+                Console.WriteLine(a);
+            }
+        }
+        public int fibonacciwithrecursion(int num)
+        {
+            if (num == 1)
+            {
+                return 0; ;
+            }
+            if (num == 2)
+            {
+                return 1;
+            }
+            else
+            {
+                return fibonacciwithrecursion(num - 1) + fibonacciwithrecursion(num - 2);
+            }
+        }
+    }
+}
+    
+
--- LogicalProblems/Program.cs
+++ LogicalProblems/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, Harshal!");
+﻿namespace LogicalProblems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("----------Welcome To Logical Program----------- ");
+            bool end = true;
+            Console.WriteLine("1.Fibonacci Series\n2.End Of The Program");
+            while (end == true)
+            {
+                Console.WriteLine("\nEnter Option For Exicute The Program");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        FibonacciSeries series = new FibonacciSeries();
+                        series.Fibonacci();
+                        series.fibonacciwithrecursion(10);
+                        break;
+                    case 3:
+                        end = false;
+                        Console.WriteLine("Program Is Ended.");
+                        break;
+                    default:
+                        Console.WriteLine("Enter The Correct Option");
+                        break;
+
+                }
+            }
+
+        }
+    }
+}
